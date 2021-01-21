8a446e143483cff68dc81af0459e459b17c189a4
German <gnrg_74@hotmail.com>
2021-01-21 16:55:42 -0600

    Se creo componente producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './Components/product/product.component';
+import { StoreComponent } from './Components/store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
