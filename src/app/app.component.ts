5838c56b8dd40a4c79c56479a893c947e775c5ef
German <gnrg_74@hotmail.com>
2021-01-20 20:06:59 -0600

    Se modificaron las imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,44 +13,44 @@ export class AppComponent {
     {
       id: '1',
       image: 'assets/image/Producto1.png',
-      title: 'Juego de baño',
-      price: 80000,
-      description: 'bla bla bla bla bla'
+      title: 'Playera',
+      price: 400,
+      description: 'Playera de Platzi para disfrutar los cursos'
     },
     {
       id: '2',
       image: 'assets/image/Producto2.png',
-      title: 'Peerfumes',
-      price: 80000,
-      description: 'bla bla bla bla bla'
+      title: 'Figura Among Us',
+      price: 1500,
+      description: 'Figura de among Us de colección'
     },
     {
       id: '3',
       image: 'assets/image/Producto3.png',
-      title: 'Aromatizantes',
-      price: 80000,
-      description: 'bla bla bla bla bla'
+      title: 'Estrella de Navidad',
+      price: 800,
+      description: 'Estrella recién salida del gabacho'
     },
     {
       id: '4',
       image: 'assets/image/Producto4.png',
-      title: 'Mariscos',
-      price: 80000,
-      description: 'bla bla bla bla bla'
+      title: 'Avion de Papel',
+      price: 450000,
+      description: 'Avión de papel de colección de él artista Shiranai Mikiju'
     },
     {
       id: '5',
       image: 'assets/image/Producto5.png',
-      title: 'Electronica',
-      price: 80000,
-      description: 'bla bla bla bla bla'
+      title: 'Corona de Flores',
+      price: 72100,
+      description: 'Corona de flores para festivales indie'
     },
     {
       id: '6',
       image: 'assets/image/Producto6.png',
-      title: 'Sabritas',
-      price: 80000,
-      description: 'bla bla bla bla bla'
+      title: 'Point Black',
+      price: 1250000,
+      description: 'Puntos negros aleatorios de colección'
     },
 
   ];
