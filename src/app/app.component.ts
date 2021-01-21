555c19cc28f8da78395075c7779cde07bdc71bd1
German <gnrg_74@hotmail.com>
2021-01-20 19:35:14 -0600

    se agrego array de producto y se tipo a la interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Product} from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +8,57 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'GamaStore';
   item = ['German', 'Andrea', 'Rosa']
+  
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/image/Producto1.png',
+      title: 'Juego de baño',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/image/Producto2.png',
+      title: 'Peerfumes',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/image/Producto3.png',
+      title: 'Aromatizantes',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/image/Producto4.png',
+      title: 'Mariscos',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/image/Producto5.png',
+      title: 'Electronica',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/image/Producto6.png',
+      title: 'Sabritas',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
 
-  addItem(){
+  ];
+
+  addItem() {
     this.item.push('nuevo item');
   }
-  deleteItem(index: number){
-    this.item.splice(index,1);
+  deleteItem(index: number) {
+    this.item.splice(index, 1);
   }
 }
