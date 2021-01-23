afdc1f54540e55b7b69db88cb95bf0352b47a2d9
German <gnrg_74@hotmail.com>
2021-01-22 18:53:01 -0600

    Se añadio atributo de prueba

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Product} from '../../product.model'
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-
+  product: Product = {
+    id: '1',
+      image: 'assets/image/Producto1.png',
+      title: 'Playera',
+      price: 400,
+      description: 'Playera de Platzi para disfrutar los cursos'
+  }
   constructor() { }
 
   ngOnInit(): void {
