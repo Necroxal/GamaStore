14c13d943b0e528007429f3998d390ff63b29e0a
German <gnrg_74@hotmail.com>
2021-02-02 17:49:57 -0600

    se crearon rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import {HomeComponent} from './Components/home/home.component';
 import {ProductsComponent} from './Components/products/products.component';
 import {ContactComponent} from './Components/contact/contact.component';
+
 const routes: Routes = [
   {
     path: "/home",
