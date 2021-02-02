de020b56519d195f13d3ee3a984f7b5c070ae9ef
German <gnrg_74@hotmail.com>
2021-02-02 17:58:47 -0600

    eliminar ciertas cosas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {HomeComponent} from './Components/home/home.component';
-import {ProductsComponent} from './Components/products/products.component';
-import {ContactComponent} from './Components/contact/contact.component';
-
-const routes: Routes = [
-  {
-    path: "/home",
-    component: HomeComponent
-  },
-  {
-    path: "/products",
-    component: ProductsComponent
-  },
-  
-  {
-    path: "/contact",
-    component: ContactComponent
-  },
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
