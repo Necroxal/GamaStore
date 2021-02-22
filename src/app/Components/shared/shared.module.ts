10bfb9f89e4596cbf9b56feb455ca2044682b3d3
German <gnrg_74@hotmail.com>
2021-02-22 13:09:47 -0600

    se creo un shared module para compartir cosas y reciclar

---
+++ src/app/Components/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExponentialPipe } from './pipes/exponentail/exponential.pipe';
+import { HighlightDirective } from './directives/highlight/highlight.directive'
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    ExponentialPipe,
+    HighlightDirective,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class SharedModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,22 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './Components/product/product.component';
-import { ExponentialPipe } from './exponential.pipe';
-import { HighlightDirective } from './highlight.directive';
 import { ProductsComponent } from './Components/products/products.component';
 import { ContactComponent } from './Components/contact/contact.component';
 import { DemoComponent } from './Components/demo/demo.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { FooterComponent } from './Components/footer/footer.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './Components/product-detail/product-detail.component';
 import { LayoutComponent } from './Components/layout/layout.component';
-
+import {SharedModule} from './Components/shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ExponentialPipe,
-    HighlightDirective,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
-    HeaderComponent,
-    FooterComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
     LayoutComponent,
@@ -36,7 +28,8 @@ import { LayoutComponent } from './Components/layout/layout.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
